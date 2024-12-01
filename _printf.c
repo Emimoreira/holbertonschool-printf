dfd00dd7e1db2b03cc54beebe84b5bf967fcfb59
BruDosSant <brudossant@gmail.com>
2024-12-01 19:54:12 +0000

    Aloha

--- _printf.c
+++ _printf.c
@@ -1,76 +1,72 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...) 
 {
 	int count = 0;
 	va_list args;
 	const char *ptr = format;
 
-	va_start(args, format);
+	if (!format)
+        {
+                return -1;
+        }
 
-	if (*ptr == '%' && *(ptr + 1) == '\0')
-	return 0;
+	va_start(args, format);
 
 	while (*ptr)
 	{
-	if (*ptr == '%')
-	{
-	ptr++;
-
-		if (*ptr == '\0')
+		if (*ptr == '%')
 		{
-		write(1, "%", 1);
-		count++;
-		break;
-		}
-
-		if (*ptr == 'c')
-		{
-		char c = (char)va_arg(args, int);
-		write(1, &c, 1);
-		count++;
-		}
-		else if (*ptr == 's')
-		{
-		char *str = va_arg(args, char*);
-		if (str == NULL)
-		{
-			write(1, "(null)", 6);
-			count += 6;
-		}
-		else
-		{
-		while (*str)
+			ptr++;
+			if (*ptr == '\0')
 			{
-			write(1, str, 1);
-			str++;
-			count++;
+				break;
+			}
+			else if (*ptr == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else if (*ptr == 'c')
+			{
+				char c = (char)va_arg(args, int);
+				write(1, &c, 1);
+				count++;
+			}
+			else if (*ptr == 's')
+			{
+				char *str = va_arg(args, char*);
+				if (str == NULL)
+				{
+					write(1, "(null)", 6);
+					count += 6;
+				}
+				else
+				{
+					while (*str)
+					{
+						write(1, str, 1);
+						str++;
+						count++;
+					}
+				}
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, ptr, 1);
+				count += 2;
 			}
 		}
-		}
-		else if (*ptr == '%')
-		{
-		write(1, "%", 1);
-		count++;
-		}
 		else
 		{
-		write(1, "%", 1);
-		write(1, ptr, 1);
-		count += 2;
+			write(1, ptr, 1);
+			count++;
 		}
-	}
-	else
-	{
-		write(1, ptr, 1);
-		count++;
+		ptr++;
 	}
 
-	ptr++;
-	}
-
-	va_end(args);
-	return (count);
+va_end(args);
+return (count);
 }
-
